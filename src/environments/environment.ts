5fdf201372e33e5c07028d315d5f08c17d29303d
KCnick <37576325+KCnick@users.noreply.github.com>
2019-07-12 12:14:23 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,7 @@
 export const environment = {
   production: false,
    firebase : {
-    apiKey: "AIzaSyCkW7ty6lQjyDzyftKch6BE42XEjBzlRbc",
-    authDomain: "kenyaairways-53bee.firebaseapp.com",
-    databaseURL: "https://kenyaairways-53bee.firebaseio.com",
-    projectId: "kenyaairways-53bee",
-    storageBucket: "",
-    messagingSenderId: "927589833585" 
+    
     
   },
 
