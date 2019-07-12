b0c7e55dbbc70c99fa06ed1c7f5462a0a07dbf90
KCnick <37576325+KCnick@users.noreply.github.com>
2019-07-12 12:14:52 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
      firebase : {
-      apiKey: "AIzaSyCkW7ty6lQjyDzyftKch6BE42XEjBzlRbc",
-      authDomain: "kenyaairways-53bee.firebaseapp.com",
-      databaseURL: "https://kenyaairways-53bee.firebaseio.com",
-      projectId: "kenyaairways-53bee",
-      storageBucket: "",
-      messagingSenderId: "927589833585"
+     
   }
 };
